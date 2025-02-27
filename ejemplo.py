a5a50291d220cc5fe205ccf4bb3ac30dcde7bdc7
TONNA <ing.mecaprogram.jvt@gmail.com>
2025-02-27 00:25:46 -0500

    papo

---
+++ ejemplo.py
@@ -0,0 +1,18 @@
+"""
+todo es un comentario:
+"""
+
+my_var = "valor"
+MY_CONSTANT = "mi const"# por convencion las constantes se escriben en mayusculas
+my_int=1
+my_float=1.0
+my_bool=True
+my_none=None
+my_list=[1,2,3]
+my_tuple=(1,2,3)
+my_string="mi cadena de texto"
+my_other_string="mi otra cadena de texto"
+print("papu")
+
+
+print(f"suma: 10+3= {10+3}")
