fbe01ef5c5285e9d9b7b11b82489a92c6dceb5b6
TONNA <ing.mecaprogram.jvt@gmail.com>
2025-03-04 16:26:17 -0500

    hi

--- variables python.py
+++ variables python.py
@@ -1,4 +1,8 @@
-rokaleta= 1
-while rokaleta<=10:
-    print(rokaleta)
-    rokaleta+=1
+def tabla_de_multiplicar(n):
+     for i in range(1,11):
+          print(n,"*",i,"=",n*i)
+tabla_de_multiplicar(5)
+ 
+if 5>4:
+    print("Aliento de gato")
+
